d081bbbd97f9bffa7d32d4e265e73e3de96ef706
Facu Olivera <f94olivera@gmail.com>
2026-04-10 09:12:16 -0300

    types for log in

---
+++ packages/types/src/auth.ts
@@ -0,0 +1,15 @@
+export interface AuthUser {
+    id: string;
+    name: string;
+    email: string;
+}
+
+export interface LoginRequest {
+    email: string;
+    password: string;
+}
+
+export interface LoginResponse {
+    token: string;
+    user: AuthUser;
+}
